6621c19bd035e30116f8ec6f4ee13e397061c662
Zayne Wang <zwang-p@outlook.com>
2024-03-12 22:50:46 +0800

    feat: add prettier support

---
+++ prettier.config.js
@@ -0,0 +1,14 @@
+/** @type {import("prettier").Config} */
+const config = {
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: false,
+  trailingComma: 'none',
+  jsxSingleQuote: false,
+  bracketSpacing: true,
+  arrowParens: 'avoid'
+};
+
+export default config;
