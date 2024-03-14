0fa2b9eec00971d44d01961176c559e7e69f3be5
Zayne Wang <zwang-p@outlook.com>
2024-03-14 09:22:17 +0800

    feat: add babel support

---
+++ babel.config.cjs
@@ -0,0 +1,28 @@
+module.exports = api => {
+  api.cache(true);
+  const isEnvProduction = process.env.NODE_ENV == "production";
+  const isEnvDevelopment = process.env.NODE_ENV == "development";
+  /**
+   * @type {import("@babel/core").TransformOptions}
+   */
+  const config = {
+    presets: [
+      [require.resolve("@babel/preset-env")],
+      require.resolve("@babel/preset-typescript")
+    ].filter(Boolean),
+    plugins: [
+      [require.resolve("@babel/plugin-proposal-decorators"), { legacy: true }],
+      [
+        require.resolve("@babel/plugin-transform-runtime"),
+        {
+          corejs: {
+            version: 3,
+            proposals: true
+          }
+        }
+      ]
+    ].filter(Boolean)
+  };
+
+  return config;
+};
